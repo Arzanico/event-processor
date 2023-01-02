54daa9bf7526d1dc20b49551acbedfff30d17c47
Nicolas Arzani <arzanico@hotmail.com>
2023-01-02 18:15:18 -0300

    initial-commit

---
+++ internal/server/event.go
@@ -0,0 +1,10 @@
+package server
+
+type Event struct {
+	Id   *string `json:"id"`
+	Type *string `json:"type"`
+}
+
+func NewEvent() *Event {
+	return &Event{}
+}
---
+++ internal/server/http.go
@@ -0,0 +1,43 @@
+package server
+
+import (
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+func NewHttpServer(addr string) *http.Server {
+	httpsrv := newHttpServer()
+	r := mux.NewRouter()
+	r.HandleFunc("/event", httpsrv.handleProduce).Methods("POST")
+	return &http.Server{
+		Addr:    addr,
+		Handler: r,
+	}
+}
+
+type httpServer struct {
+	Event *Event
+}
+
+func newHttpServer() *httpServer {
+	return &httpServer{
+		Event: NewEvent(),
+	}
+}
+
+func (s *httpServer) handleProduce(w http.ResponseWriter, r *http.Request) {
+	var event Event
+	err := json.NewDecoder(r.Body).Decode(&event)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	res := event
+	err = json.NewEncoder(w).Encode(res)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+}
