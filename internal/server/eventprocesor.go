fcec09da049d8cdde3b87c8f785c0245478349da
Nicolas Arzani <arzanico@hotmail.com>
2023-01-06 12:34:34 -0300

    min refactor

--- internal/server/eventprocesor.go
+++ internal/server/eventprocesor.go
@@ -20,25 +20,25 @@ type RealTimeRequest struct{}
 
 func (r RealTimeRequest) ProcessEvent(event Event) (string, error) {
 	fmt.Println("Define RealTime behavior")
-	fmt.Printf("Event id : %s", event.Id)
-	fmt.Printf("Event type : %s", event.Type)
+	fmt.Printf("Event id : %s\n", event.Id)
+	fmt.Printf("Event type : %s\n", event.Type)
 	return "done", nil
 }
 
 type PushNotification struct{}
 
 func (p PushNotification) ProcessEvent(event Event) (string, error) {
-	fmt.Println("Define RealTime behavior")
-	fmt.Printf("Event id : %s", event.Id)
-	fmt.Printf("Event type : %s", event.Type)
+	fmt.Println("Define Pushnotification behavior")
+	fmt.Printf("Event id : %s\n", event.Id)
+	fmt.Printf("Event type : %s\n", event.Type)
 	return "done", nil
 }
 
 type InitialUpload struct{}
 
 func (i InitialUpload) ProcessEvent(event Event) (string, error) {
-	fmt.Println("Define RealTime behavior")
-	fmt.Printf("Event id : %s", event.Id)
-	fmt.Printf("Event type : %s", event.Type)
+	fmt.Println("Define Initial upload behavior")
+	fmt.Printf("Event id : %s\n", event.Id)
+	fmt.Printf("Event type : %s\n", event.Type)
 	return "done", nil
 }
